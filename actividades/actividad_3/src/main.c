d894a5dcceaf9b6a8f38bc1dbf227bd44f2dde16
Fabrizio Carlassara <fabrizio.carlassara@gmail.com>
2024-06-24 08:25:05 -0300

    chore: proyecto de platformio

---
+++ actividades/actividad_3/src/main.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "pico/stdlib.h"
+
+/**
+ * @brief Programa principal
+*/
+int main(void) {
+	// Inicializo el USB
+	stdio_init_all();
+	// Demora para esperar la conexion
+	sleep_ms(1000);
+
+	// Inicializacion de GPIO con gpio_init()
+
+	// Configuracion de entrada/salida con gpio_set_dir()
+
+
+	while (true) {
+
+		// Resolver logica para GPIO20 -> GPIO6
+
+		// Resolver logica para GPIO21 -> GPIO7
+
+		// Resolver logica para GPIO22 -> GPIO8
+
+	}
+	return 0;
+}
